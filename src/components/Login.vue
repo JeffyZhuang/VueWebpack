079a93357e85c029844b80ba4fd484e9c93217d5
JeffyZhuang <270225738@qq.com>
2019-01-05 17:39:47 +0800

    提交login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>{{ msg }}</h1>
+</div>
+</template>
+ 
+<script>
+export default {
+    name: '登录',
+    data() {
+        return {
+            msg: 'welcome login'
+ 
+        }
+        
+    }
+}</script>
