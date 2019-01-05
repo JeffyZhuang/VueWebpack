cc9a5438e493d20d195d92210413aaa22468e827
JeffyZhuang <270225738@qq.com>
2019-01-05 17:38:45 +0800

    新增登录vue界面 src里面的组件，main.js中组件与路由的交互，以及App.vue中的呈现方式，index.js中的路由选择

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
+const Login = resolve => require(['@/components/Login'], resolve)
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '登录',
+      component: Login
     }
   ]
 })
